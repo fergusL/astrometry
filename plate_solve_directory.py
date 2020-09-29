73222f003633d5298059dfba87dd93acd1857f08
fergus <fergus.longbottom@hdr.mq.edu.au>
2020-09-29 16:41:57 +1000

    added some documentation

--- plate_solve_directory.py
+++ plate_solve_directory.py
@@ -11,22 +11,29 @@ from functools import partial
 import tqdm
 
 
-def solve_field_task(options, fname):
+def solve_field_task(options, fpath):
+    """Small wrapper function to run get_solve_field() and ignore any timeouts.
+
+    Args:
+        options (dict): dictionary containing one keyword 'solve_opts' which maps
+                        to a list of solve-field arguments
+        fpath (str): The filepath of the file to be processed by solve-field
+    """
     try:
-        get_solve_field(fname, **options)
+        get_solve_field(fpath, **options)
     except subprocess.TimeoutExpired:
-        print(f'solve-field timed out on {fname}.')
+        print(f'solve-field timed out on {fpath}.')
         pass
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--path', type=str, default="./")
-    parser.add_argument('--timeout', type=int, default=30)
-    parser.add_argument('--downsample', type=int, default=10)
-    parser.add_argument('--scalelow', type=int, default=1)
-    parser.add_argument('--scalehigh', type=int, default=2)
-    parser.add_argument('--nproc', type=int, default=1)
+    parser.add_argument('--path', type=str, default="./", help="Directory to search for files for processing.")
+    parser.add_argument('--timeout', type=int, default=30, help="The time out length (S) for each solve-field job.")
+    parser.add_argument('--downsample', type=int, default=10, hepl="Downsample images by an integer factor, makes processing faster")
+    parser.add_argument('--scalelow', type=int, default=1, help="Set minimum scale (deg) for solve-field to look for features.")
+    parser.add_argument('--scalehigh', type=int, default=2, help="Set maximum scale (deg) for solve-field to look for features.")
+    parser.add_argument('--nproc', type=int, default=1, help="Number of processors to use in multiprocessing.")
     args = parser.parse_args()
 
     fpaths = []
